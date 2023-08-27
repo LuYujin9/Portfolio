8d20b91965bf7af53ed6e24f52e7a1f91882e6f6
LuYujin9 <wuwujane@hotmail.com>
2023-08-27 17:55:54 +0200

    wrote the elements in projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,15 +1,42 @@
+import "./Projects.css";
+
 function Projects() {
   return (
     <main>
-      <article className="" id="">
-        <h2>Project</h2>
-        <h3>Lecker Map</h3>
-        <p>
+      <article>
+        <h3>PROJEKTS</h3>
+        <p className="project-description">
           Lecker Map is a full stack App for restaurant browsing and reserving.
           It's my capstone project from the neue fische boot camp.
         </p>
-        <ul>
-          <li>
+      </article>
+      <div classNam="filter">
+        <button>All</button>
+        <button>Javascript</button>
+        <button>React</button>
+        <button>Typescript</button>
+        <button>PHP</button>
+      </div>
+      <ul className="projects-container">
+        <li className="project-card">
+          <h4>Lecker Map</h4>
+          <p className="project-description">
+            Lecker Map ist mein Capstone-Project von intensivem Kurs für
+            Web-develop. Es ist eine full-sack-App and biete die functions:
+            Restaurants Anschauen, Reservieren, Kommentieren und in Map schauen.
+            Außerdem kann man noch die Kommentars und Reserves sich verändern
+            und entfern.
+          </p>
+          <img
+            src="/images/lecker-map.jpg"
+            alt="foto of project"
+            className="project-foto"
+          />
+          <p className="project-description">
+            <strong>Tech Stack: </strong>Next.js, React, Javascript, MangoDB,
+            Mangoose, testing-library/react
+          </p>
+          <div className="project-link-container">
             <a
               href="https://github.com/LuYujin9/capstone-project"
               target="_blank"
@@ -17,8 +44,6 @@ function Projects() {
             >
               Code
             </a>
-          </li>
-          <li>
             <a
               href="https://capstone-project-luyujin9.vercel.app/"
               target="_blank"
@@ -26,9 +51,79 @@ function Projects() {
             >
               Vorschau
             </a>
-          </li>
-        </ul>
-      </article>
+          </div>
+        </li>
+        <li className="project-card">
+          <h4>Lecker Map</h4>
+          <p className="project-description">
+            Lecker Map ist mein Capstone-Project von intensivem Kurs für
+            Web-develop. Es ist eine full-sack-App and biete die functions:
+            Restaurants Anschauen, Reservieren, Kommentieren und in Map schauen.
+            Außerdem kann man noch die Kommentars und Reserves sich verändern
+            und entfern.
+          </p>
+          <img
+            src="/images/lecker-map.jpg"
+            alt="foto of project"
+            className="project-foto"
+          />
+          <p className="project-description">
+            <strong>Tech Stack: </strong>Next.js, React, Javascript, MangoDB,
+            Mangoose, testing-library/react
+          </p>
+          <div className="project-link-container">
+            <a
+              href="https://github.com/LuYujin9/capstone-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Code
+            </a>
+            <a
+              href="https://capstone-project-luyujin9.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Vorschau
+            </a>
+          </div>
+        </li>
+        <li className="project-card">
+          <h4>Lecker Map</h4>
+          <p className="project-description">
+            Lecker Map ist mein Capstone-Project von intensivem Kurs für
+            Web-develop. Es ist eine full-sack-App and biete die functions:
+            Restaurants Anschauen, Reservieren, Kommentieren und in Map schauen.
+            Außerdem kann man noch die Kommentars und Reserves sich verändern
+            und entfern.
+          </p>
+          <img
+            src="/images/lecker-map.jpg"
+            alt="foto of project"
+            className="project-foto"
+          />
+          <p className="project-description">
+            <strong>Tech Stack: </strong>Next.js, React, Javascript, MangoDB,
+            Mangoose, testing-library/react
+          </p>
+          <div className="project-link-container">
+            <a
+              href="https://github.com/LuYujin9/capstone-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Code
+            </a>
+            <a
+              href="https://capstone-project-luyujin9.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Vorschau
+            </a>
+          </div>
+        </li>
+      </ul>
     </main>
   );
 }
