2dd8fc45355e1e8899c66ddb0fe5bc92ef461f37
LuYujin9 <wuwujane@hotmail.com>
2023-08-29 20:14:27 +0200

    correct the animation in homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../components/style/Homepage.css";
 
 function Home() {
+  const [isStart, setIsStart] = useState(false);
+
+  useEffect(() => {
+    setIsStart(true);
+  }, []);
   return (
     <main>
       <section className="homepage">
-        <div className="homepage-context-container">
-          <h2>Yujin Lu</h2>
-          <h5>
-            <strong>
-              Ich bin eine Web-Entwicklerin in Dresden mit chinesischen Wurzeln.
-            </strong>
-          </h5>
-          <Link className="about-me-link" to="/about">
-            ABOUT ME
-          </Link>
+        <div className={`${isStart ? "slide-up" : "slide-up-active"}`}>
+          <div className="homepage-context-container">
+            <h2>Yujin Lu</h2>
+            <h5>
+              <strong>
+                Ich bin eine Web-Entwicklerin in Dresden mit chinesischen
+                Wurzeln.
+              </strong>
+            </h5>
+            <Link className="about-me-link" to="/about">
+              ABOUT ME
+            </Link>
+          </div>
         </div>
       </section>
     </main>
