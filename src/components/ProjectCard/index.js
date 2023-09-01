3cf0b8a0c5a42f760c4fb2481e28120d1e96387c
LuYujin9 <wuwujane@hotmail.com>
2023-09-01 10:26:07 +0200

    update the ProjectCard, wenn there is not preview, the link won't be showed

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -13,22 +13,26 @@ function ProjectCard({ project }) {
     <li className={`project-card ${isStart ? "enlarge" : "enlarge-active"}`}>
       <h4>{project.name}</h4>
       <div className="project-link-container">
-        <a
-          href={project.code}
-          target="_blank"
-          rel="noreferrer"
-          className="project-link"
-        >
-          Code
-        </a>
-        <a
-          href={project.preview}
-          target="_blank"
-          rel="noreferrer"
-          className="project-link"
-        >
-          Vorschau
-        </a>
+        {project.code !== "" && (
+          <a
+            href={project.code}
+            target="_blank"
+            rel="noreferrer"
+            className="project-link"
+          >
+            Code
+          </a>
+        )}
+        {project.preview !== "" && (
+          <a
+            href={project.preview}
+            target="_blank"
+            rel="noreferrer"
+            className="project-link"
+          >
+            Vorschau
+          </a>
+        )}
       </div>
       <p className="project-description">{project.description}</p>
       <img src={project.photo} alt="from project" className="project-photo" />
