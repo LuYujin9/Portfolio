8b7278e52825e29082ee408e669a89b5b66db34e
LuYujin9 <wuwujane@hotmail.com>
2023-09-01 10:50:36 +0200

    put the variables of data in data.js

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -2,48 +2,13 @@ import { useState, useEffect } from "react";
 import ProjectCard from "../components/ProjectCard";
 import Filter from "../components/Filter";
 import "../components/style/Projects.css";
-
-const filterConditions = ["All", "Javascript", "PHP", "MangoDB", "MySQL"];
-
-const projectsData = [
-  {
-    name: "Lecker Map",
-    description:
-      "Die Lecker Map ist mein Capstone-Project aus dem intensivkurs für Web-development. Es ist eine full-sack-App mit folgenden Funktionen: Restaurants Suche nach eigenen Kriterien, Vorschau(Location,Menü etc.), Reservierung und Möglichkeit für Kommentare.",
-    photo: "/images/lecker-map.png",
-    techStack:
-      "Next.js, React, Javascript, MangoDB, Mangoose,styled-components, testing-library/react",
-    keyWord: ["Javascript", "MangoDB"],
-    code: "https://github.com/LuYujin9/capstone-project",
-    preview: "https://capstone-project-luyujin9.vercel.app/",
-  },
-  {
-    name: "Portfolio",
-    description:
-      "Die Portfolio Webseite zeigt meine Informationen, um mich besser kennenlernen zu können.",
-    photo: "/images/portfolio.png",
-    techStack: "Javascript, React, styled-components",
-    keyWord: ["Javascript", "React"],
-    code: "https://github.com/LuYujin9/Portfolio",
-    preview: "https://potfolio-gray.vercel.app/",
-  },
-  {
-    name: "Feedback Page",
-    description:
-      "Das ist eine Webseite, um PHP und MySQL zu lernen und zu üben.",
-    photo: "/images/Feedback.png",
-    techStack: "PHP, MySQL, phpMyAdmin",
-    keyWord: ["PHP", "MySQL"],
-    code: "https://github.com/LuYujin9/PHP_Project_Feedback_Page",
-    preview: "",
-  },
-];
+import { filterConditions, projectsData } from "../data/data.js";
 
 function Projects() {
   const [projects, setProjects] = useState(projectsData);
   const [fitlerActive, setFitlerActive] = useState("All");
   const [isStart, setIsStart] = useState(false);
-
+  console.log(filterConditions);
   function handleProjectsList(condition) {
     setFitlerActive(condition);
     if (condition === "All") {
